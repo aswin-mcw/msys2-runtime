36f72e2ac89f3c7dbc21a7a79da7d1bc95cec78d
Radek Bartoň <radek.barton@microsoft.com>
2025-06-04 14:55:34 +0200

    Cygwin: signal: make context structures registers handling portable
    This patch extracts macros from winsup/cygwin/exceptions.cc serving for portable
    register access to context structures into a separate local header
    winsup/cygwin/local_includes/register.h and implements their AArch64
    counterparts.
    
    Then, it adds AArch64 declaration of __mcontext structure based on
    mingw-w64-headers/include/winnt.h header to
    winsup/cygwin/include/cygwin/singal.h header.
    
    Then, it includes the registers.h header and uses the macros where applicable,
    namely at:
     - winsup/cygwin/exceptions.cc
     - winsup/cygwin/profil.c
     - winsup/cygwin/tread.cc
    
    The motivation is to make usage of the context structures portable without
    unnecessary #if defined(__x86_64__) while implementations of signal handling
    code will be developed later, e.g. implementation of makecontext.
    
    Signed-off-by: Radek Bartoň <radek.barton@microsoft.com>
    (cherry picked from commit 017a156f8a2d0817e74d0e79c47ea24c57c77a3e)

--- winsup/cygwin/thread.cc
+++ winsup/cygwin/thread.cc
@@ -32,6 +32,7 @@ details. */
 #include "ntdll.h"
 #include "cygwait.h"
 #include "exception.h"
+#include "register.h"
 
 /* For Linux compatibility, the length of a thread name is 16 characters. */
 #define THRNAMELEN 16
@@ -629,27 +630,7 @@ pthread::cancel ()
       threadlist_t *tl_entry = cygheap->find_tls (cygtls);
       if (!cygtls->inside_kernel (&context))
 	{
-#if defined(__x86_64__)
-	  /* Need to maintain the alignment of the stack pointer.
-	     https://learn.microsoft.com/en-us/cpp/build/stack-usage?view=msvc-170
-	     states,
-	       "The stack will always be maintained 16-byte aligned,
-		except within the prolog (for example, after the return
-		address is pushed),",
-	     that is, we need 16n + 8 byte alignment here because the stack
-	     pointer must be maintaiined to the same alignment required by
-	     the function prologue. Since the call instruction pushes the
-	     return address (rip) onto the stack, which is 8 bytes,
-	     an additional 8 bytes is required to emulate this behaviour.
-	     However, we do not need to push return address itself, because
-	     pthread::static_cancel_self() must not return. */
-	  context.Rsp &= ~0x07UL;
-	  if ((context.Rsp & 8) == 0)
-	    context.Rsp -= 8;
-	  context.Rip = (ULONG_PTR) pthread::static_cancel_self;
-#else
-#error unimplemented for this target
-#endif
+	  context._CX_instPtr = (ULONG_PTR) pthread::static_cancel_self;
 	  SetThreadContext (win32_obj_id, &context);
 	}
       cygheap->unlock_tls (tl_entry);
