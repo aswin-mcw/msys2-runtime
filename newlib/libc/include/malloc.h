7badf6b7c122ff1c2bcbb06d83b4b874751984a3
aswin-mcw <aswin.kalies@multicorewareinc.com>
2026-03-31 13:21:32 +0530

    newlib : mstats macro comment

--- newlib/libc/include/malloc.h
+++ newlib/libc/include/malloc.h
@@ -140,7 +140,7 @@ extern void __malloc_unlock(struct _reent *);
 /* A compatibility routine for an earlier version of the allocator.  */
 
 extern void mstats (char *);
-#if defined(__CYGWIN__) || defined(__MSYS__)
+#if defined(__CYGWIN__)
 #undef _mstats_r
 #define _mstats_r(r, p) mstats (p)
 #else
